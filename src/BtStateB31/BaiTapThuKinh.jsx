f9c818f0fa6aa3f065e56b948527f26fed21ee69
QUY666666 <lenhatquy2401@gmail.com>
2024-08-31 20:38:37 +0700

    update img n

--- src/BtStateB31/BaiTapThuKinh.jsx
+++ src/BtStateB31/BaiTapThuKinh.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import dataGlasses from "../Data/dataGlasses.json";
 import Modal from "../../public/ImgGlasses/model.jpg";
 import GlassesV1 from "../../public/ImgGlasses/v1.png";
 
 const BaiTapThuKinh = () => {
+  const renderGlassesList = () => {
+    return dataGlasses.map((glassesItem, index) => {
+      return (
+        <>
+          <img src={glassesItem.url} style={{ width: "100px" }} alt="" />
+        </>
+      );
+    });
+  };
+
   const styleGlasses = {
     width: "150px",
-    top: "117px",
+    top: "80px",
     right: "116px",
     opacity: "0.6",
+  };
+  const infoGlasses = {
+    width: "250px",
+    top: "178px",
+    left: "318px",
+    paddingLeft: "10px",
+    backgroundColor: "rgba(255,127,0,0.5)",
+    textAlign: "left",
+    height: "127px",
   };
   return (
     <div
@@ -25,19 +45,19 @@ const BaiTapThuKinh = () => {
       >
         <h3
           style={{ backgroundColor: "rgba(0,0,0,0.3)" }}
-          className="text-center text-white p-5"
+          className="text-center text-white p-3"
         >
           TRY GLASSES APP ONLINE
         </h3>
         <div className="container">
-          <div className="row text-center">
+          <div className="row mt-5 text-center">
             <div className="col-6">
               <div className="position-relative">
                 <img
                   src={Modal}
                   alt=""
                   style={{ width: "250px" }}
-                  className="mt-5 position-absolute"
+                  className=" position-absolute"
                 />
                 <img
                   src={GlassesV1}
@@ -45,15 +65,20 @@ const BaiTapThuKinh = () => {
                   className="position-absolute"
                   alt=""
                 />
+                <div
+                  className="position-relative fw-bold text-white"
+                  style={infoGlasses}
+                >
+                  <p className="">Tên kính:</p>
+                  <p>Mô tả:</p>
+                </div>
               </div>
             </div>
             <div className="col-6">
-              <img
-                src={Modal}
-                alt=""
-                style={{ width: "250px" }}
-                className="mt-5"
-              />
+              <img src={Modal} alt="" style={{ width: "250px" }} className="" />
+            </div>
+            <div className="bg-white container text-center mt-3">
+              {renderGlassesList()}
             </div>
           </div>
         </div>
